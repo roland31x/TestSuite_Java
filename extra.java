4e0de1fcb5c200ac95f4a0a294755f1db362fe1a
roland31x <roland31x@gmail.com>
2024-07-02 19:30:00 +0000

    Add some extra functions

--- extra.java
+++ extra.java
@@ -1,73 +1,104 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Extra {
 
-    public static String randomString(int length) {
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder sb = new StringBuilder();
-        Random rand = new Random();
-        for(int i = 0; i < length; i++) {
-            sb.append(chars.charAt(rand.nextInt(chars.length())));
+    public static boolean isPrime(int num) {
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
         }
-        return sb.toString();
+        return true;
     }
 
-    public static long factorial(int n) {
-        if(n == 0) return 1;
-        return n * factorial(n - 1);
-    }
-
-    public static boolean isPalindrome(String s) {
-        String str = s.toLowerCase().replaceAll("\\s+", "");
-        return new StringBuilder(str).reverse().toString().equals(str);
-    }
-
-    public static <T> List<T> uniqueElements(List<T> list) {
-        return new ArrayList<>(new HashSet<>(list));
-    }
-
-    public static <T> List<T> flattenList(List<List<T>> nestedList) {
-        return nestedList.stream().flatMap(Collection::stream).collect(Collectors.toList());
-    }
-
-    public static long fibonacci(int n) {
-        long a = 0, b = 1;
-        for(int i = 0; i < n; i++) {
-            long temp = a;
-            a = b;
-            b = temp + b;
+    public static int gcd(int a, int b) {
+        while(b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
         }
         return a;
     }
 
-    public static long countVowels(String s) {
-        long count = 0;
-        for(char c : s.toLowerCase().toCharArray()) {
-            if("aeiou".indexOf(c) >= 0) count++;
-        }
-        return count;
+    public static int lcm(int a, int b) {
+        return a * (b / gcd(a,b));
     }
 
-    public static Map<String, String> mergeMaps(Map<String, String>... maps) {
-        Map<String, String> result = new HashMap<>();
-        for(Map<String, String> map : maps) {
-            result.putAll(map);
+    public static Map<String, Object> flattenMap(Map<String, Object> map) {
+        Map<String, Object> result = new HashMap<>();
+        flattenMapHelper("", map, result);
+        return result;
+    }
+
+    private static void flattenMapHelper(String prefix, Map<String, Object> map, Map<String, Object> result) {
+        for (String key : map.keySet()) {
+            Object val = map.get(key);
+            String newKey = prefix.isEmpty() ? key : prefix + "." + key;
+            if (val instanceof Map) {
+                flattenMapHelper(newKey, (Map<String,Object>) val, result);
+            } else {
+                result.put(newKey, val);
+            }
+        }
+    }
+
+    public static <T> T mostCommon(List<T> list) {
+        Map<T, Integer> freq = new HashMap<>();
+        T maxKey = null;
+        int maxCount = 0;
+        for(T item : list) {
+            freq.put(item, freq.getOrDefault(item, 0) + 1);
+            if(freq.get(item) > maxCount) {
+                maxCount = freq.get(item);
+                maxKey = item;
+            }
+        }
+        return maxKey;
+    }
+
+    public static <T> List<List<T>> transpose(List<List<T>> matrix) {
+        List<List<T>> result = new ArrayList<>();
+        if(matrix.isEmpty()) return result;
+        int rows = matrix.size();
+        int cols = matrix.get(0).size();
+        for(int i=0; i<cols; i++) {
+            List<T> col = new ArrayList<>();
+            for(int j=0; j<rows; j++) {
+                col.add(matrix.get(j).get(i));
+            }
+            result.add(col);
         }
         return result;
     }
 
-    public static String reverseWords(String sentence) {
-        return Arrays.stream(sentence.split(" "))
-                     .map(w -> new StringBuilder(w).reverse().toString())
-                     .collect(Collectors.joining(" "));
+    public static String camelToSnake(String s) {
+        return s.replaceAll("([A-Z])", "_$1").toLowerCase();
     }
 
-    public static <T> List<List<T>> chunkList(List<T> list, int chunkSize) {
-        List<List<T>> chunks = new ArrayList<>();
-        for(int i = 0; i < list.size(); i += chunkSize) {
-            chunks.add(list.subList(i, Math.min(i + chunkSize, list.size())));
+    public static String swapCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(Character.isUpperCase(c)) sb.append(Character.toLowerCase(c));
+            else sb.append(Character.toUpperCase(c));
         }
-        return chunks;
+        return sb.toString();
+    }
+
+    public static int nestedSum(List<?> list) {
+        int sum = 0;
+        for(Object o : list) {
+            if(o instanceof List) sum += nestedSum((List<?>) o);
+            else if(o instanceof Number) sum += ((Number) o).intValue();
+        }
+        return sum;
+    }
+
+    public static int digitSum(int num) {
+        num = Math.abs(num);
+        int sum = 0;
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
